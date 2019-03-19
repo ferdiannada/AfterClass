44ce5fc13f71f110fc92ef04ce354c11e287e5e9
Ropan <seesaajeen2@gmail.com>
2019-03-19 15:11:01 +0700

    Iki tekku lho yo
    awas

---
+++ P2/rofan/application/controllers/Tidakbisa.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * 
+ */
+class Tidakbisa extends CI_Controller	{
+	
+	public function index()	
+	{
+		echo "<h1> Mau ngakses apa coba?</h1>";
+	}
+}
+
