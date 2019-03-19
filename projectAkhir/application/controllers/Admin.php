d0f22001e071936c7a5e15808de6bb52d64a2969
ferdiannada <44969814+ferdiannada@users.noreply.github.com>
2019-03-19 14:24:13 +0700

    lupa

--- projectAkhir/application/controllers/Admin.php
+++ projectAkhir/application/controllers/Admin.php
@@ -1,11 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Awal extends CI_Controller{
+class Admin extends CI_Controller{
 
   function index()
   {
-    $this->load->view('index');
+    $this->load->view('admin/index');
   }
 
 }
