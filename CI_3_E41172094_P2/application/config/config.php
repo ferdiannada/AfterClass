20a5a6e0dedeec415b30c49cb99d9cc33516569b
ferdiannada <44969814+ferdiannada@users.noreply.github.com>
2019-03-29 19:22:14 +0700

    buat dari awal sampai belajar CRUD

--- CI_3_E41172094_P2/application/config/config.php
+++ CI_3_E41172094_P2/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/P2/ferdian/';
+$config['base_url'] = 'http://localhost/CI_3_E41172094_P2/';
 
 /*
 |--------------------------------------------------------------------------
