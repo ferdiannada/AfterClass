0985b51036754b429a05c38156562c016d660a43
ferdiannada <44969814+ferdiannada@users.noreply.github.com>
2019-03-29 20:03:14 +0700

    tampil data dari database

---
+++ CI.3.E41172094_P2/application/views/vlistBarang.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Data Barang</title>
+  </head>
+  <body>
+
+      <h1><?php echo $judul; ?></h1>
+
+
+
+        <table border="1">
+          <tr>
+            <th>ID Barang</th>
+            <th>Nama Barang</th>
+            <th>Stok</th>
+            <th>Harga</th>
+          </tr>
+          <?php foreach ($barang as $b): ?>
+            <tr>
+                <td><?= $b->id_barang; ?></td>
+                <td><?= $b->nama_barang; ?></td>
+                <td><?= $b->stok; ?></td>
+                <td><?= $b->harga; ?></td>
+            </tr>
+          <?php endforeach; ?>
+        </table>
+
+
+
+
+  </body>
+</html>
