2f01848a05e1f835598bd50da179abfd30e4ec0a
Rizmawan <rizmawanwidi@gmail.com>
2019-04-05 00:46:45 +0700

    update rizmawan

--- CI.3.E41172111_P3/application/models/m_barang.php
+++ CI.3.E41172111_P3/application/models/m_barang.php
@@ -1,10 +1,18 @@
 <?php
-    class m_barang extends CI_Model
-    {
-        private $_table="barang";
-        function list_barang()
-        {
-            return $this->db->get($this->_table)->result();
-        }
+class m_barang extends CI_Model{
+    public function list_barang(){
+        return $this->db->get('barang');
     }
-?>
+public function input_data($data,$table){
+    $this->db->insert($table,$data);
+}
+
+function hapus_barang($kdbarang){
+    return $hasil=$this->db->query("DELETE FROM barang WHERE kdbarang='$kdbarang'");
+}
+
+function edit_barang($data, $table) 
+{
+      $this->db->update($table, $data);
+}
+}
