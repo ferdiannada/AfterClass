2e7c38aa03103951677597d270252cf84741a36f
Rizmawan <rizmawanwidi@gmail.com>
2019-05-12 11:33:44 +0700

    anjai banget

--- projectakhir/application/config/config.php
+++ projectakhir/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/afterClass/furnitur/';
+$config['base_url'] = 'http://localhost/projectakhir/';
 
 /*
 |--------------------------------------------------------------------------
