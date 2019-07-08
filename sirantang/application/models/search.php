8cc7780346d66569d0f1184cb8d3f83764eca8a0
ferdiannada <ferdian.nadda@gmail.com>
2019-07-08 16:01:01 +0700

    hapus sirantang

--- sirantang/application/models/search.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-    class search extends CI_Model {
-
-        function ambil_data($table){
-            return $this->db->get($table);
-        }
-    }
